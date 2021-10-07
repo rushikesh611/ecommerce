2fb1efa8acaecaf62ca82e8c5a9f7edd3445de5f
Hrushikesh Jadhav <rushi611@gmail.com>
2021-10-07 22:20:49 +0530

    Slider

---
+++ client/src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://image.freepik.com/free-photo/happy-long-haired-woman-smiling-after-shopping-portrait-pleased-white-girl-with-paper-bags_197531-8240.jpg",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF ON NEW ARRIVALS",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://image.freepik.com/free-photo/young-attractive-dark-haired-woman-with-short-haircut-with-lot-shopping-bags_295783-12503.jpg",
+    title: "WINTER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 15% OFF ON NEW ARRIVALS",
+    bg: "ffffff",
+  },
+  {
+    id: 3,
+    img: "https://image.freepik.com/free-photo/woman-with-shopping-bags-studio-yellow-background-isolated_1303-14294.jpg",
+    title: "NEW ARRIVALS",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 10% OFF ON NEW ARRIVALS",
+    bg: "fbf0f4",
+  },
+];
