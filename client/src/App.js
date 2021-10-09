3a1158c1de8f2badb0e3a25b727c16cff4981a93
Hrushikesh Jadhav <rushi611@gmail.com>
2021-10-09 13:12:17 +0530

    cart page

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,9 @@ import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 function App() {
-  return <Login />;
+  return <Cart />;
 }
 
 export default App;
