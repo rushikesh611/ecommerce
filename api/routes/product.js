39fa8eb5794faf5c9d7010e5376abc514392021e
Hrushikesh Jadhav <rushi611@gmail.com>
2021-10-22 17:42:31 +0530

    fetch and filter products

--- api/routes/product.js
+++ api/routes/product.js
@@ -8,6 +8,7 @@ router.post("/", verifyTokenAndAdmin, async (req, res) => {
   const newProduct = new Product(req.body);
   try {
     const savedProduct = await newProduct.save();
+    // res.header("Access-Control-Allow-Origin", "*");
     res.status(200).json(savedProduct);
   } catch (err) {
     res.status(500).json(err);
